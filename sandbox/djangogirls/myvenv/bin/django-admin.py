f9a85c75579adabf4473fc344106f34b1381d4c3
Rachael Carson <rachaelcarson@gmail.com>
2018-01-05 17:28:29 +1100

    Adding all sandbox files

---
+++ sandbox/djangogirls/myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/RachaelandMatt/Documents/Rachael/sandbox/djangogirls/myvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
